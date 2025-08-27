8fa8afa52b761efffdc6ec276221618c75817e89
zuzjak <zuzanna.jakubas05@gmail.com>
2025-08-27 20:13:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Proste zachowanie dla linków nawigacyjnych: płynne przewijanie
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', (e) => {
+    const id = a.getAttribute('href').slice(1);
+    const el = document.getElementById(id);
+    if (el) {
+      e.preventDefault();
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
